6c24d9655dc7cc89d51bca4fe924fa90734fbe96
Artem Kopytko <artem.kop19@gmail.com>
2023-07-27 21:52:07 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _2240._Number_of_Ways_to_Buy_Pens_and_Pencils
+{
+    internal class Program
+    {
+        static public long WaysToBuyPensPencils(int total, int cost1, int cost2)
+        {
+            long result = 0;
+            for(int i = 0; i <= total / cost1; i++) 
+            {
+                result += ((total - cost1 * i) / cost2) + 1;
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
